730f14b1e406394a94a2b5f6d8175bf133552e93
jaeseopark <>
2020-02-27 21:23:36 -0700

    cleaned up imports and variable names

--- gdrivewrapper/_gdrivewrapper.py
+++ gdrivewrapper/_gdrivewrapper.py
@@ -1,11 +1,11 @@
 from multiprocessing import Lock
 
-from gdrivewrapper._core import *
+from gdrivewrapper.core import *
 
 _lock = Lock()
 
 
-def lock_and_call(func, *args, **kwargs):
+def _lock_and_call(func, *args, **kwargs):
     _lock.acquire()
     try:
         return func(*args, **kwargs)
@@ -18,13 +18,13 @@ class GDriveWrapper:
         self.svc = get_service_object(scopes, creds_path)
 
     def upload(self, *args, **kwargs):
-        return lock_and_call(upload, self.svc, *args, **kwargs)
+        return _lock_and_call(upload, self.svc, *args, **kwargs)
 
     def download_bytes(self, *args, **kwargs):
-        return lock_and_call(download_bytes, self.svc, *args, **kwargs)
+        return _lock_and_call(download_bytes, self.svc, *args, **kwargs)
 
     def create_folder(self, *args, **kwargs):
-        return lock_and_call(create_folder, self.svc, *args, **kwargs)
+        return _lock_and_call(create_folder, self.svc, *args, **kwargs)
 
     def create_comment(self, *args, **kwargs):
-        return lock_and_call(create_comment, self.svc, *args, **kwargs)
+        return _lock_and_call(create_comment, self.svc, *args, **kwargs)
